59ba25c2fbc830ed1436dc413e6d8000c2f49ff3
Rafaela Carvalho <carvalho.sims@gmail.com>
2024-06-15 16:04:01 -0300

    para-sala

---
+++ exercicios/para-sala/exer1.py
@@ -0,0 +1,8 @@
+#1. Faça um programa que peça para o usuário inserir uma idade e mostre na tela se ele é maior de idade ou não.
+
+idade = int(input('Insira sua idade: '))
+
+if idade >= 18:
+    print('Você é maior de idade ')
+else:
+    print('Você é menor de idade ')
---
+++ exercicios/para-sala/exer2.py
@@ -0,0 +1,14 @@
+#2. Faça um programa que mostre na tela uma pergunta de múltipla escolha, e que, a partir da resposta do usuário, mostre na tela se ele acertou ou não.
+
+
+print("Qual a melhor professora de lógica do mundo ? \n a)Agnes \n b)Lucia \n c)Maria \n d)Não existe")
+resposta = input("insira a reposta certa: ")
+
+if resposta == "a":
+    print("Respota Certa!!")
+else:
+     print("resposta Errada")
+
+
+
+#\n
