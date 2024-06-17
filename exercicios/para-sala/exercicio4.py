225eba250053749df36e250ae422fa690ee1c57e
Dyliane Souza <dyliane@hotmail.com>
2024-06-17 16:48:50 -0300

    exercícios de sala att

--- exercicios/para-sala/exercicio4.py
+++ exercicios/para-sala/exercicio4.py
@@ -1,25 +1,21 @@
 # Faça um programa que pergunte ao usuário se ele possui irmãos, e que, caso a resposta seja “sim”, 
 # pergunte quantos e mostre na tela uma mensagem de sua escolha. No caso de o usuário responder “não”, 
 # pergunte se ele gostaria de ter e mostre na tela uma mensagem de sua escolha.
-# def tem_irmaos(resposta):
-#     if resposta == 'sim':
-#         return input('Quantos irmãos você tem? ')
-#     else: 
-#         return input('Você gostaria de ter irmãos? ')
+# 
+
+def tem_irmaos(resposta):
+    if resposta == 'sim':
+        input('Que legal! Quantos irmãos você tem? ')
+    else: 
+        respostadois= input('Você gostaria de ter irmãos? ')
+        if respostadois == 'sim':
+                print('Que bom! Ter irmãos é super legal')
+        else: 
+                print('Entendo. Ter irmão às vezes é complicado')
     
 
-
-# respostaInserida = input('Você tem irmaos? ')
-
 resposta = input('Você tem irmaos? ')
+print(tem_irmaos(resposta))
 
-if resposta == 'Sim':
-    resposta_positiva = input ('Que legal! Quantos irmãos você tem?')
-    sua_resposta = f'Você tem {resposta_positiva} irmãos'
-    print(sua_resposta)
-else:
-    resposta_negativa= input('Que pena! Você gostaria de ter?')
-    sua_resposta_neg = f"Obrigada por sua resposta!"
-    print(sua_resposta_neg)
 
 
--- exercicios/para-sala/exercicio5.py
+++ exercicios/para-sala/exercicio5.py
@@ -1,8 +1,5 @@
 # Faça um programa que permita o usuário escolher entre três opções de bebidas e mostre na tela a bebida escolhida.
 
-# opcoes_bebida = input('Qual bebida você deseja?\n a)Gin  b)Aguá  c)Vodka\nDigite sua resposta: ')
-# bebida_escolhida = f'A sua bebida escolhida foi {opcoes_bebida}'
-# print(bebida_escolhida)
 
 def escolher_bebida(opcoes_bebida):
     if opcoes_bebida == 'gin': 
