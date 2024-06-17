83016684762c24c45757ef78d5a3d54016e47836
unknown <rafaellaroden@gmail.com>
2024-06-17 19:32:47 -0300

    atualizando exercicio 1

--- exercicios/para-sala/exercicio1.py
+++ exercicios/para-sala/exercicio1.py
@@ -1,17 +1,18 @@
 # 1. Faça um programa que peça para o usuário inserir uma idade e mostre na tela se ele é maior de idade ou não.
 
+
 def verifica_idade(idade):
     if idade >= 18:
         return 'Usuário maior de idade'
     else:
         return 'Usuário menor de idade'
     
-        
 try:
     idade_inserida = int(input("insira sua idade: "))
     resultado = verifica_idade(idade_inserida)
     print (resultado)
-except: ValueError
+except ValueError:
+    print('O campo de idade precisa ser preenchido com dados numericos')
 
 
 
