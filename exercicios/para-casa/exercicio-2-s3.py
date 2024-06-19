3f6b151402d0b50daacb260f54757ae3322f5ec1
Karine Lessa <karinelessa00@gmail.com>
2024-06-19 20:40:43 -0300

    exercicio-s3-att

--- exercicios/para-casa/exercicio-2-s3.py
+++ exercicios/para-casa/exercicio-2-s3.py
@@ -4,6 +4,23 @@
  ##Faça um programa que mostre ao usuario um menu com 4 opções de operações matemáticas (as básicas, por exemplo). O usuário escolhe uma das opçõoes e o seu programa então pede dois valores numéricos e realiza a operação, mostrando o resultado.
 
 
+def  operacao_adicao(numero1, numero2):
+    return numero1 + numero2
+def operacao_subtracao(numero1, numero2):
+    return numero1 - numero2
+def operacao_multiplicacao(numero1, numero2):
+    return numero1 * numero2
+def operacao_divisao(numero1, numero2):
+    return numero1 / numero2
+
+
+def menu_operacao():
+    print("Menu:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
 
 
 
