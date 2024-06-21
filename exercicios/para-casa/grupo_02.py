3538ac1f3f6010d6aca8a869afa6c77b48f7469e
Paloma <palomaavena@gmail.com>
2024-06-21 10:58:46 -0300

    Update grupo_02.py

--- exercicios/para-casa/grupo_02.py
+++ exercicios/para-casa/grupo_02.py
@@ -4,6 +4,28 @@
 # (as básicas, por exemplo). O usuário escolhe uma das opçõoes e o seu programa 
 # então pede dois valores numéricos e realiza a operação, mostrando o resultado.
 
+def operacao (menu_escolha, numero1, numero2):
+    if menu_escolha == "1" or menu_escolha == "adição":
+        resultado= numero1 + numero2
+    elif menu_escolha == "2" or menu_escolha == "subtração":
+        resultado= numero1 - numero2
+    elif menu_escolha == "3" or menu_escolha == "divisão":
+        resultado= numero1 / numero2
+    elif menu_escolha == "4" or menu_escolha == "multiplicação":
+        resultado=numero1*numero2
+    else:
+        print("Operação inválida!")
+        resultado = 0
+    return resultado    
+
+menu_escolha= input("Escolha uma opção, das operações da matemática abaixo: \n 1.adição \n 2.subtração \n 3.divisão \n 4.multiplicação\n: ")
+numero1 = float(input("Coloque um número:"))
+numero2= float(input("Coloque o segundo número:"))
+
+
+resultado = operacao(menu_escolha, numero1, numero2)
+print("Resultado: ", resultado)
+
 #* Faça um programa para verificar se um determinado número inteiro e divisível por 3 ou 5,
 #  mas não simultaneamente pelos dois.
 
@@ -13,5 +35,3 @@
 # • Ou ter trabalhado pelo menos 30 anos,
 #• Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos
 
-print "Escolha uma opção, das operações da matemática abaixo: \n 1.adição \n 2.subtração \n 3.divisão \n 4.multiplicação"
-def operacao ()
